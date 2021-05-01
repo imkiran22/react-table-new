891fa3fe800b1e94d51fc8e7dcea9027ddbe5987
imkiran22 <kiran22292@gmail.com>
2021-05-01 19:48:06 +0530

    All routes added

--- src/components/Customers/SingleCustomer.tsx
+++ src/components/Customers/SingleCustomer.tsx
@@ -3,6 +3,5 @@ import { useParams } from "react-router-dom";
 
 export const SingleCustomer: React.FC<{}> = () => {
   const { id }: any = useParams();
-  alert(id);
   return id ? <h1>Welcome User {id}</h1> : <h1>ADD USER</h1>;
 };
